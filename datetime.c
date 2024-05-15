9f3b23e1020b642901f2c9dc3e895e2dbb053a60
Antenor Leal <antenor.leal@oceanpact.com>
2024-05-15 13:08:37 -0300

    leftpad date in log

--- datetime.c
+++ datetime.c
@@ -4,6 +4,6 @@ char* datetime() {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
     char *ret = malloque(25);
-    sprintf(ret, "%d/%d/%d %d:%d:%d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    sprintf(ret, "%02d/%02d/%d %02d:%02d:%02d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
     return ret;
 }
