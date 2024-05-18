e5b7bdf8e3cba0c1b898e63a846deb5e4b979d80
antenor-z <antenor-z@pm.me>
2024-05-18 17:16:31 -0300

    .

--- loadTest.py
+++ loadTest.py
@@ -4,31 +4,28 @@ from time import sleep
 import requests
 import random
 
-urls = {'http://localhost:5000': 0, 
-        'http://localhost:5000/aaa': 0, 
-        'http://localhost:5000/a.html': 0,
-        'http://localhost:5000/s.html': 0}
+urls = [('http://localhost:5000', 27), 
+        ('http://localhost:5000/aaa', 42), 
+        ('http://localhost:5000/a.html', 12),
+        ('http://localhost:5000/s.html', 19)]
 
-def make_request():
-    try:
-        url = random.choice(list(urls.keys()))
-        requests.get(url, timeout=3)
-        print(url)
-        urls[url] += 1
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
+def make_request(url, number_of_hits):
+    for _ in range(int(number_of_hits)):
+        try:
+            requests.get(url, timeout=3)
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+        # wait between 0.0001 and 0.1 s
+        sleep(random.randrange(1, 100) / 1000)
 
 threads = []
 
-for _ in range(100):
-    for _ in range(15):
-        thread = threading.Thread(target=make_request)
-        threads.append(thread)
-        thread.start()
-    sleep(0.1)
+for (url, number_of_hits) in urls:
+    thread = threading.Thread(target=make_request, args=(url, number_of_hits))
+    threads.append(thread)
+    thread.start()
 
 for thread in threads:
     thread.join()
 
-print(json.dumps(urls, indent=1))
 print("OK")
