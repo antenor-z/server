e4a70c3c48f993ce392229e3267a27e191b751a5
antenor-z <antenor-z@pm.me>
2024-06-10 20:42:59 -0300

    extra malloc because of new line

--- debug.c
+++ debug.c
@@ -18,7 +18,7 @@ void debug(Queue* queue, const char* message, ...) {
         int len = vsnprintf(NULL, 0, message, args_copy);
         va_end(args_copy);
 
-        char* log = malloque(len + 1);
+        char* log = malloque(len + 2); // Because of \n
         vsprintf(log, message, args);
         strcat(log, "\n");
         enqueue(queue, log);
