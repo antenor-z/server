e2676306063c5d40c2f3a55251cc3425522ee501
antenor-z <antenor-z@pm.me>
2024-06-10 21:38:08 -0300

    .

--- utils.c
+++ utils.c
@@ -49,18 +49,12 @@ void help() {
 
 /*
  * Safe malloc
- * Allocates and zeros the memory
- * If malloc is unsuccessful panic the program, this way I don't need to
- * check for NULL ptr each time I allocate memory
- * I zero the memory to avoid buffer overflow even if I forget to null
- * terminate the string
  */
 void *malloque(size_t size) {
     void* ptr = malloc(size);
     if (ptr == NULL) {
         panic(1, "malloc failed");
     }
-    memset(ptr, '\0', size);
     return ptr;
 }
 
